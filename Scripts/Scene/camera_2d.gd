ecb6a6d0f433cc17e08ab4769e0ad6e0f6c1ce01
matheusnajal <matheusnajalk@gmail.com>
2024-10-13 21:26:59 -0300

    effects

---
+++ Scripts/Scene/camera_2d.gd
@@ -0,0 +1,60 @@
+extends Camera2D
+
+@export var randomStrength: float = 30.0
+@export var shakeFade: float = 5.0
+@export var min_time: float = 0.0
+@export var max_time: float = 5.0
+@export var shake_duration: float = 15.0
+@export var max_shake_strength: float = 10.0
+
+var rng = RandomNumberGenerator.new()
+
+var shake_Strength: float = 0.0
+var has_shaken: bool = false
+var is_shaking: bool = false
+
+func _ready() -> void:
+	start_random_shake_timer()
+
+func start_random_shake_timer():
+	var random_time = rng.randf_range(min_time, max_time)
+	
+	# Configura um Timer para o shake
+	var shake_timer = Timer.new()
+	shake_timer.wait_time = random_time
+	shake_timer.one_shot = true
+	shake_timer.connect("timeout", Callable(self, "_on_shake_timeout"))
+	add_child(shake_timer)
+	shake_timer.start()
+
+func _on_shake_timeout():
+	if not has_shaken:
+		apply_shake(randomStrength)
+		has_shaken = true
+		
+		var duration_timer = Timer.new()
+		duration_timer.wait_time = shake_duration
+		duration_timer.one_shot = true
+		duration_timer.connect("timeout", Callable(self, "_on_shake_end"))
+		add_child(duration_timer)
+		duration_timer.start()
+
+func apply_shake(intensity: float):
+	shake_Strength = clamp(intensity, 0, max_shake_strength)  # Limita a intensidade para um valor máximo
+	is_shaking = true
+
+func adjust_shake_intensity(new_intensity: float):
+	shake_Strength = clamp(new_intensity, 0, max_shake_strength)
+
+func _on_shake_end():
+	is_shaking = false
+
+func _process(delta: float) -> void:
+	if is_shaking:
+		offset = randomOffset()
+	elif shake_Strength > 0:
+		shake_Strength = lerpf(shake_Strength, 0, shakeFade * delta)
+		offset = randomOffset()
+
+func randomOffset() -> Vector2:
+	return Vector2(rng.randf_range(-shake_Strength, shake_Strength), rng.randf_range(-shake_Strength, shake_Strength))
